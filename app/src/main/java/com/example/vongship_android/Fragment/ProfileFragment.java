9fbfa2ba638d87ae0e43d3d80678da842fcaf7b1
Khoa <khoa4755@gmail.com>
2020-12-04 22:23:30 +0700

    edit nofitication + detail profile

--- app/src/main/java/com/example/vongship_android/Fragment/ProfileFragment.java
+++ app/src/main/java/com/example/vongship_android/Fragment/ProfileFragment.java
@@ -4,14 +4,21 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 
+import com.example.vongship_android.Activity.DetailProfileFeature;
+import com.example.vongship_android.Activity.DetailProfileStore;
+import com.example.vongship_android.Activity.LoginActivity;
+import com.example.vongship_android.Activity.NotificationsDetailActivity;
+import com.example.vongship_android.Activity.RegisterActivity;
 import com.example.vongship_android.Class.DownloadImageTask;
 import com.example.vongship_android.R;
 import com.google.firebase.auth.FirebaseAuth;
@@ -19,6 +26,8 @@ import com.google.firebase.auth.FirebaseUser;
 
 
 public class ProfileFragment extends Fragment {
+    private TextView txtStore;
+    private TextView txtFeature;
     private FirebaseAuth auth;
     private FirebaseAuth.AuthStateListener authListener;
     @Nullable
@@ -26,6 +35,25 @@ public class ProfileFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_profile,container,false);
         TextView txt_name= (TextView) root.findViewById(R.id.nameTv);
+
+        txtStore = (TextView)root.findViewById(R.id.detailstore);
+        txtStore.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getActivity(), DetailProfileStore.class);
+                ProfileFragment.this.startActivity(intent);
+            }
+        });
+
+        txtFeature = (TextView)root.findViewById(R.id.detailfeature);
+        txtFeature.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getActivity(), DetailProfileFeature.class);
+                ProfileFragment.this.startActivity(intent);
+            }
+        });
+
 
         auth = FirebaseAuth.getInstance();
         final FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
